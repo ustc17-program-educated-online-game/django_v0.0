39fc5ce6f83f2b489c93cc72e31f068fae3c5282
ling-0-sys <kicc1234@qq.ccom>
2020-06-01 17:07:24 +0800

    Register the models about game in game/admin.py

--- django_sys/game/admin.py
+++ django_sys/game/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . import models
+
+admin.site.register(models.HistoryInfo)
+admin.site.register(models.PlayerInfo)
+admin.site.register(models.Map)
